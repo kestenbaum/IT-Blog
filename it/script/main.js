cbc12d4294ba71ce820a05be650545fbbb937ba0
Vovnenko Alexei <kespa71for@gmail.com>
2021-01-27 18:57:11 +0200

    version 1.1

--- it/script/main.js
+++ it/script/main.js
@@ -1,1 +1,18 @@
 'use strict'
+
+let burger = document.querySelector('.burger'),
+    header = document.querySelector('.header__right'),
+    headerLink = document.querySelectorAll('.header__link')
+
+burger.onclick = function (){
+    burger.classList.toggle('burger__active')
+    header.classList.toggle('header__active')
+}
+
+for (let i of headerLink){
+    i.onclick = function (){
+        burger.classList.remove('burger__active')
+        header.classList.remove('header__active')
+    }
+}
+
