4f9ac6bdbf768c35cf26e8171049585c00a9d830
Vovnenko Alexei <kespa71for@gmail.com>
2021-01-28 22:55:31 +0200

    version 1.5.3

--- it/script/main.js
+++ it/script/main.js
@@ -3,8 +3,9 @@
 let burger = document.querySelector('.burger'),
     header = document.querySelector('.header__right'),
     headerLink = document.querySelectorAll('.header__link'),
-    input = document.querySelector('.input_sub'),
-    btnSub = document.querySelector('.btn__sub')
+    search = document.querySelector('.search__link'),
+    searchLine = document.querySelector('.search__line'),
+    searchFooter = document.querySelector('.search__footer')
 
 
 burger.onclick = function (){
@@ -17,5 +18,18 @@ for (let i of headerLink){
         burger.classList.remove('burger__active')
         header.classList.remove('header__active')
     }
+}
+
+
+search.onclick = function (){
+    searchLine.classList.toggle('search__active')
+    header.classList.remove('header__active')
+    burger.classList.remove('burger__active')
+}
+
+searchFooter.onclick = function (){
+    searchLine.classList.toggle('search__active')
+    header.classList.remove('header__active')
+    burger.classList.remove('burger__active')
 }
 
