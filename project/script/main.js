a4f2333c73b4a0d943e50f6ba8ea3a5a1ef0125b
Vovnenko Alexei <kespa71for@gmail.com>
2021-03-13 07:42:19 +0200

    relize

--- project/script/main.js
+++ project/script/main.js
@@ -1,5 +1,5 @@
 'use strict'
-let  popup = document.querySelector('.popup'),
+let popup = document.querySelector('.popup'),
     burger = document.querySelector('.burger__wrap'),
     headerNav = document.querySelector('.header-list')
 
@@ -24,37 +24,37 @@ const swiper = new Swiper('.swiper-container', {
     },
 });
 
-burger.onclick = function (){
+burger.onclick = function() {
     burger.classList.toggle('burger-active')
     headerNav.classList.toggle('nav-active')
 }
 
 
 
-$(document).ready(function (){
+$(document).ready(function() {
     $('.tab__item').not(':first').hide();
-    $('.tabs__wrapper .tab').click(function (){
+    $('.tabs__wrapper .tab').click(function() {
         $('.tabs__wrapper .tab').removeClass('active').eq($(this).index()).addClass('active');
         $('.tab__item').hide().eq($(this).index()).fadeIn()
     }).eq(0).addClass('active')
 })
 
-window.onclick = function (a){
+window.onclick = function(a) {
     let target = a.target
-    if (target.classList.contains('popup-btn')){
+    if (target.classList.contains('popup-btn')) {
         popup.classList.add('popup-active')
     }
-    if (target.classList.contains('popup')){
+    if (target.classList.contains('popup')) {
         popup.classList.remove('popup-active')
     }
-    if (target.classList.contains('header-link')){
+    if (target.classList.contains('header-link')) {
         headerNav.classList.remove('nav-active')
         burger.classList.remove('burger-active')
     }
 }
 
-$(function () {
-    $.fn.setCursorPosition = function (pos) {
+$(function() {
+    $.fn.setCursorPosition = function(pos) {
         if ($(this).get(0).setSelectionRange) {
             $(this).get(0).setSelectionRange(pos, pos);
         } else if ($(this).get(0).createTextRange) {
@@ -66,27 +66,143 @@ $(function () {
         }
     };
     $.mask.definitions['N'] = '[/0-6|9/]';
-    $('input[type="tel"]').click(function () {
+    $('input[type="tel"]').click(function() {
         $(this).setCursorPosition(3);
     }).mask("+7(N99) 999-99-99");
 });
 
-// $(function() {
-//
-//     $('.simple-ajax-popup-align-top').magnificPopup({
-//         type: 'ajax',
-//         callbacks: {
-//             ajaxContentAdded: function() {
-//                 var currentUrl = window.location.href;
-//                 var nclass = document.querySelectorAll('.politic');
-//                 for (var i = 0; i < nclass.length; i++) {
-//                     nclass[i].innerHTML = '<a href="' + currentUrl + '">' + currentUrl + '</a>';
-//                 }
-//             }
-//         },
-//         fixedContentPos: true,
-//         fixedBgPos: true,
-//         overflowY: 'auto'
-//     });
-//
-// });
+
+
+
+function removeLocationHash() {
+    let noHashURL = window.location.href.replace(/#.*$/, '');
+    window.history.replaceState('', document.title, noHashURL)
+}
+window.addEventListener("popstate", function(event) {
+    removeLocationHash();
+});
+window.addEventListener("hashchange", function(event) {
+    event.preventDefault();
+    removeLocationHash();
+});
+window.addEventListener("load", function() {
+    removeLocationHash();
+});
+
+//==== Gallery ====
+$(function() {
+
+    $('.tab').on('click', function() {
+
+        $('.potfolio__slider.slick-initialized').slick("setPosition");
+
+        $('.potfolio__slider__nav.slick-initialized').slick("setPosition");
+
+    });
+
+    $('.potfolio__slider').slick({
+
+        slidesToShow: 1,
+
+        slidesToScroll: 1,
+
+        autoplay: false,
+
+        autoplaySpeed: 3000,
+
+        pauseOnHover: true,
+
+        infinite: true,
+
+        fade: true,
+
+        asNavFor: '.potfolio__slider__nav'
+
+    });
+
+    $('.potfolio__slider__nav').slick({
+
+        slidesToShow: 5,
+
+        slidesToScroll: 1,
+
+        arrows: false,
+
+        autoplay: false,
+
+        infinite: true,
+
+        asNavFor: '.potfolio__slider',
+
+        dots: false,
+
+        centerMode: true,
+
+        focusOnSelect: true,
+
+        responsive: [
+
+            {
+
+                breakpoint: 480,
+
+                settings: {
+
+                    slidesToShow: 3,
+
+                }
+
+            }
+
+            // You can unslick at a given breakpoint now by adding:
+
+            // settings: "unslick"
+
+            // instead of a settings object
+
+        ]
+
+    });
+
+});
+
+$(document).ready(function() {
+
+    $('.popup-gallery').magnificPopup({
+
+        delegate: 'a',
+
+        type: 'image',
+
+        tLoading: 'Loading image #%curr%...',
+
+        mainClass: 'mfp__portfolio__popup',
+
+        gallery: {
+
+            enabled: true,
+
+            navigateByImgClick: true,
+
+            fixedContentPos: true,
+
+            fixedBgPos: true,
+
+            overflowY: 'auto',
+
+            preload: [0, 1], // Will preload 0 - before current, and 1 after the current image
+
+            arrowMarkup: '', // markup of an arrow button
+
+            tPrev: 'Назад', // title for left button
+
+            tNext: 'Next (Right arrow key)', // title for right button
+
+            tCounter: '%curr% из %total%'
+
+        }
+
+    });
+
+});
+//==== /Gallery ====
