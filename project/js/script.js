005f34ab76856ce98834a14aee45dde40e25ff6e
Vovnenko Alexei <kespa71for@gmail.com>
2021-03-29 15:19:36 +0300

    relize

--- project/js/script.js
+++ project/js/script.js
@@ -5,7 +5,7 @@
 new Swiper('.slider-container', {
     slidesPerView: 4,
    // Optional parameters
-  /*  loop: true,
+   loop: true,
    slidesPerView: 1,
    spaceBetween: 30,
    breakpoints: {
@@ -15,7 +15,7 @@ new Swiper('.slider-container', {
         1148: {
             slidesPerView: 4,
         },
-    }, */
+    },
  });
 //==== /slider ====
 
@@ -29,6 +29,8 @@ new Swiper('.slid-cont', {
     },
     slidesPerView: 1,
     spaceBetween: 30,
+    autoHeight: 1,
+    simulateTouch: false,
     breakpoints: {
         800: {
             slidesPerView: 1,
@@ -44,24 +46,70 @@ new Swiper('.slid-cont', {
  //==== scroll ====
  
  window.onscroll = function(){
-    let header = document.querySelector('.header-top')
-
+    let header = document.querySelector('.header-top'),
+        headTop = document.querySelector('.header-act')
+        
     if (window.pageYOffset > 0){
         header.classList.add('header-top__active')
+        header.classList.add('header-scale')
     } else {
         header.classList.remove('header-top__active')
+        header.classList.remove('header-scale')
+        headTop.classList.add('header-scale') 
+        if (window.pageYOffset == 0) {
+            header.classList.add('header-top__active')
+            header.classList.remove('header-scale')
+        }
     }
  }
+
  //==== /scroll ==== 
 
 
+//==== popup ====
+let popup = document.querySelector('.popup'), 
+    quiz = document.querySelectorAll('.helper-test__item'), 
+    btnNext = document.querySelector('.btn-next'),
+    btnPrev = document.querySelector('.btn-prev')
 
-//==== checkbox ====
+
 window.onclick = function(a) {
     let target = a.target
-    if (target.classList.contains('design-list')) {
-        console.log('a');     
+    if (target.classList.contains('popup-btn')) {
+        popup.classList.add('popup-active')                
+    }
+    if (target.classList.contains('popup')) {
+        popup.classList.remove('popup-active')
+    }
+    if (target.classList.contains('helper-test__item')) {
+        target.classList.toggle('quiz-active')  
+        btnNext.disabled = false
+    }
+    if (target.classList.contains('btn-next')) { 
+        btnNext.disabled = true     
+    }
+    if (target.classList.contains('checkbox-item')) {  
+        btnNext.disabled = false     
+    }
+    if (target.classList.contains('btn-prev')) {  
+        btnNext.disabled = false     
+    }
+    if (target.classList.contains('design-list') || target.classList.contains('design-list__text') ) {              
     }
 }
-//==== /checkbox ====
+//==== /popup ====
 
+
+
+//==== list ====
+let linkList = document.querySelectorAll('.dlist__item')
+
+for (let i=0; i<linkList.length; i++){
+    let subMenu = linkList[i].nextElementSibling;
+    let arrow = linkList[i];
+    linkList[i].addEventListener('click', function(){
+        subMenu.classList.toggle('display-block')
+    })
+}
+
+//==== /list ====
